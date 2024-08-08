a1300e29a2f30fd68c12483d0a8a6e0d26b6023c
andihaki <andi.hakim.arif@gmail.com>
2024-08-08 14:21:07 +0700

    chore: setup react query n api

---
+++ app/api/getIteneraries.ts
@@ -0,0 +1,24 @@
+async function getData() {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        // Authorization:
+        //   "Bearer ...",
+      },
+    };
+  
+    const response = fetch(
+      "https://pandooin.com/api/zamrood/itinerary?highlight=true",
+      options
+    )
+      .then((response) => response.json())
+      .catch((err) => console.error(err));
+  
+    return response;
+  }
+  
+  export default async function getIteneraries() {
+    const data = await getData();
+    return data;
+  }
