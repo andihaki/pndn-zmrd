d74f381f634c618294f0423452a6597c979cdf63
andihaki <andi.hakim.arif@gmail.com>
2024-08-09 14:07:24 +0700

    fix: active menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,10 +5,12 @@ import Image from "next/image";
 import { Menu } from "lucide-react";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import HeaderMenu from "./HeaderMenu";
+import HeaderMenu, { MENUS } from "./HeaderMenu";
 import clsx from "clsx";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
+  const [hash, setHash] = useState(() => window.location.hash || "#");
   const [isScrolled, setIsScrolled] = useState(false);
   const handleScroll = () => {
     if (window.scrollY > 160 && !isScrolled) {
@@ -58,7 +60,27 @@ export default function Header() {
               <Menu className={clsx(isScrolled && "text-brand-gray")} />
             </SheetTrigger>
             <SheetContent className={clsx(isScrolled && "text-brand-gray")}>
-              <HeaderMenu isMobile />
+              <ul className="flex flex-col lg:flex-row gap-2">
+                {MENUS.map((menu) => (
+                  <li
+                    key={menu.name}
+                    className={clsx(
+                      "px-4 py-4 hover:bg-white hover:text-black",
+                      menu.link === hash && "border-b-2"
+                    )}
+                    onClick={() => setHash(menu.link)}
+                  >
+                    <SheetTrigger>
+                      <a href={menu.link}>{menu.name}</a>
+                    </SheetTrigger>
+                  </li>
+                ))}
+              </ul>
+              <Button
+                className={clsx("px-6 py-4", isScrolled && "text-brand-gray")}
+              >
+                Need Assistance?
+              </Button>
             </SheetContent>
           </Sheet>
         </div>
--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -1,6 +1,28 @@
+"use client";
+
 import clsx from "clsx";
 
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export const MENUS = [
+  {
+    name: "Homepage",
+    link: "#",
+  },
+  {
+    name: "Customize Your Trip",
+    link: "#customize-your-trip",
+  },
+  {
+    name: "Destination",
+    link: "#destination",
+  },
+  {
+    name: "Article",
+    link: "#article",
+  },
+];
 
 type Props = {
   isMobile?: boolean;
@@ -8,23 +30,23 @@ type Props = {
 };
 
 export default function HeaderMenu({ isMobile, isScrolled }: Props) {
+  const [hash, setHash] = useState(() => window.location.hash || "#");
   return (
     <>
-      {/* @todo: create constant list of header */}
-      <a href="#">
-        <div className="px-6 py-4 border-b-2 hover:bg-white hover:text-black">
-          Homepage
-        </div>
-      </a>
-      <a href="#customize-your-trip">
-        <div className="px-6 py-4">Customize Your Trip</div>
-      </a>
-      <a href="#destination">
-        <div className="px-6 py-4">Destination</div>
-      </a>
-      <a href="#article">
-        <div className="px-6 py-4">Article</div>
-      </a>
+      <ul className="flex flex-col lg:flex-row gap-2">
+        {MENUS.map((menu) => (
+          <li
+            key={menu.name}
+            className={clsx(
+              "px-4 py-4 hover:bg-white hover:text-black",
+              menu.link === hash && "border-b-2"
+            )}
+            onClick={() => setHash(menu.link)}
+          >
+            <a href={menu.link}>{menu.name}</a>
+          </li>
+        ))}
+      </ul>
       <Button
         className={clsx(
           "px-6 py-4",
